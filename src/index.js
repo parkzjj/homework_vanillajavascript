15d2f1d0379b5e5b5f168c475e28231e73bfad80
parkzjj <113297319+parkzjj@users.noreply.github.com>
2022-10-04 13:15:22 +0800

    Update index.js
    처음 창을 열었을 때 기본배경색 하늘색 유지하되, 창이 바뀔때마다 배경색이 바뀌게 설정함

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ const body = document.querySelector("body");
 const colors = ["#87ceeb", "#800080", "#ffff00"];
 
 
-function changeColor() {
-  body.classList.remove("originbg");
+function handleColor() {
+  // body.classList.remove("originbg");
   if (window.innerWidth <= 600) {
-    body.classList.add("originbg");
+    // body.classList.remove("originbg");
+    body.style.backgroundColor = colors[0];
   } else if (window.innerWidth > 600 && window.innerWidth < 900) {
+    // body.classList.remove("originbg");
     body.style.backgroundColor = colors[1];
   } else {
+    // body.classList.remove("originbg");
     body.style.backgroundColor = colors[2];
   }
 }
 
-window.addEventListener("resize", changeColor);
+// body.style.backgroundColor = colors[0];
+window.addEventListener("resize", handleColor);
 
+// 1.처음부터 css로 기본 배경색을 주고 시작하거나
+// 2.처음부터 js로 기본 배경색을 주고 시작하거나
